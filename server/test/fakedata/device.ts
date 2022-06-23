4c8ea404905840bb40ed4a35ecdc3b2bd9246603
Junior Paz Formoso <junior.presagios@gmail.com>
2022-06-23 14:25:42 +0300

    Added device fake data

---
+++ server/test/fakedata/device.ts
@@ -0,0 +1,17 @@
+export default [
+    {
+      uid: 1,
+      vendor: "vendorB",
+      status: "online",
+    },
+    {
+      uid: 3,
+      vendor: "vendorA",
+      status: "offline",
+    },
+    {
+        uid: 66,
+        vendor: "vendorAB",
+        status: "offline",
+      }
+  ];
