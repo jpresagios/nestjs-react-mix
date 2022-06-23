851f5dd8ba44bbf447c8abe575e19c507ca88a75
Junior Paz Formoso <junior.presagios@gmail.com>
2022-06-23 13:53:14 +0300

    Added config to read enviroment

---
+++ client/src/configs/env-config.ts
@@ -0,0 +1,8 @@
+import * as dotenv from "dotenv";
+
+dotenv.config({});
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  REACT_APP_URL: process.env.REACT_APP_URL,
+};
