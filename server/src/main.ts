175cf04efcd8599463dd01b18ac1410366263992
Junior Paz Formoso <junior.presagios@gmail.com>
2022-06-24 10:58:33 +0300

    Added global pipeline error BadRequestException

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { BadRequestException, ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
 import { SwaggerModule } from '@nestjs/swagger';
 import { swaggerOptions } from './configs/swagger.config';
@@ -9,7 +9,9 @@ import env from './configs/env-config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    exceptionFactory: (errors) => new BadRequestException(errors)
+  }));
 
   const document = SwaggerModule.createDocument(app, swaggerOptions);
   SwaggerModule.setup('/swagger', app, document);
