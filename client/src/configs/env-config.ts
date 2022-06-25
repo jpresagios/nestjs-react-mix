11b7b3adb5b958a39036cb42490252ef2139715b
Junior Paz Formoso <junior.presagios@gmail.com>
2022-06-25 04:19:54 +0300

    Refactored to read enviroment variable

--- client/src/configs/env-config.ts
+++ client/src/configs/env-config.ts
@@ -1,7 +1,3 @@
-import * as dotenv from 'dotenv';
-
-dotenv.config({});
-
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   REACT_APP_URL: process.env.REACT_APP_URL,
--- client/src/services/gateway.ts
+++ client/src/services/gateway.ts
@@ -1,13 +1,18 @@
 import axios from 'axios';
 import { GatewayI, DevicePayloadI } from '../interfaces/gateway';
+import env from '../configs/env-config';
 
 // eslint-disable-next-line import/prefer-default-export
-export const getGateways = () => axios.get('http://localhost:3000/gateway');
 
-export const createGateway = (payload: GatewayI) => axios.post('http://localhost:3000/gateway', payload);
+// create axios config services
 
-export const getGatewayDetail = (id: string | undefined) => axios.get(`http://localhost:3000/gateway/${id}`);
+const domain = env.REACT_APP_URL;
+export const getGateways = () => axios.get(`${domain}/gateway`);
 
-export const createDevice = (payload: DevicePayloadI) => axios.post('http://localhost:3000/device', payload);
+export const createGateway = (payload: GatewayI) => axios.post(`${domain}/gateway`, payload);
 
-export const deleteDevice = (id: string) => axios.delete(`http://localhost:3000/device/${id}`);
+export const getGatewayDetail = (id: string | undefined) => axios.get(`${domain}/gateway/${id}`);
+
+export const createDevice = (payload: DevicePayloadI) => axios.post(`${domain}/device`, payload);
+
+export const deleteDevice = (id: string) => axios.delete(`${domain}/device/${id}`);
