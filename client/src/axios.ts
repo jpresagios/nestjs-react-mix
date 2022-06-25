0eab278d1b8686c016d9702900ad8123ecb19005
ismaelrr888 <ismaelrr888@gmail.com>
2022-06-25 12:45:16 +0300

    removed response interceptor axios

--- client/src/axios.ts
+++ client/src/axios.ts
@@ -1,5 +1,4 @@
 import axios, { AxiosInstance } from 'axios';
-import { message as $message } from 'antd';
 import env from './configs/env-config';
 
 const axiosInstance: AxiosInstance = axios.create({
@@ -11,28 +10,6 @@ axiosInstance.interceptors.request.use(
   (config) => config,
   (error) => {
     Promise.reject(error);
-  },
-);
-
-axiosInstance.interceptors.response.use(
-  (config) => {
-    if (config?.data?.message) {
-      // $message.success(config.data.message)
-    }
-    return config;
-  },
-  (error) => {
-    let errorMessage = 'Oops something went wrong';
-    if (error?.message?.includes('Network Error')) {
-      errorMessage = 'Network error, check it out';
-      $message.error(errorMessage);
-    } else {
-      errorMessage = error?.message;
-    }
-    return {
-      status: error?.response?.status,
-      message: error?.response?.data?.class ? error.response.data.class : errorMessage,
-    };
   },
 );
 
